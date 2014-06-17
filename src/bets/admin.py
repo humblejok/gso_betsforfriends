eba2fa932e3636aee490ef6e2ba786631825a96c
De Jonckheere Stéphane <humble.jok@gmail.com>
2014-06-17 06:14:48 +0200

    Admin

--- src/bets/admin.py
+++ src/bets/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from bets.models import Score, Match, Group, BettableEvent, EventRanking,\
-    UserRanking, Provider, ProviderMapping
+    UserRanking, Provider, ProviderMapping, Winner, WinnerSetup
 
 admin.site.register(BettableEvent)
 admin.site.register(EventRanking)
 admin.site.register(UserRanking)
+admin.site.register(Winner)
+admin.site.register(WinnerSetup)
 admin.site.register(Provider)
 admin.site.register(ProviderMapping)
 admin.site.register(Group)
